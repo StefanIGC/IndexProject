6165c6972339b22861f06f80d60224f60b74a3ef
Stefan Chiciuc <stefanchiciuc@gmail.com>
2023-09-28 21:30:31 +0300

    ISV3-165 Configured User Model, Devise JWT and JTI

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  Rails.application.routes.draw do
+    devise_for :users, path: 'api/auth', path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      registration: 'signup'
+    },
+    controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
+    }
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'home#index'
 end
