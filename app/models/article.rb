c8c9e7fe56ec4120e03eb3791ed22eeb2c6d9ccb
Stefan Chiciuc <stefanchiciuc@gmail.com>
2023-09-29 00:23:00 +0300

    ISV3-165 Use faker for seeding data

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
-  validates :title , presence: true, length: { minimum: 5, maximum: 20 }
-  validates :description, presence: true, length: { minimum: 10, maximum: 200 }
+  validates :title , presence: true, length: { minimum: 5, maximum: 50 }
+  validates :description, presence: true, length: { minimum: 5, maximum: 300 }
 
   belongs_to :author, class_name: 'User'
   belongs_to :category
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,51 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Article.delete_all
+Comment.delete_all
+User.delete_all
+Category.delete_all
+Tag.delete_all
+
+
+
+5.times do
+    Category.create!(
+        name: Faker::Commerce.product_name,
+    )
+end
+
+10.times do
+    Tag.create!(
+        name: Faker::Superhero.name
+    )
+end
+
+
+# db/seeds.rb
+20.times do
+    User.create!(
+      email: Faker::Internet.email,
+      password: "secrete",
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name
+    )
+end
+  
+
+30.times do
+  Article.create!(
+    title: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph,
+    category_id: Category.all.sample.id,
+    tag_id: Tag.all.sample.id,
+    author_id: User.all.sample.id
+  )
+end
+
+
+50.times do
+  Comment.create!(
+    content: Faker::Lorem.paragraph,
+    rating: rand(1.0..5.0),
+    user_id: User.all.sample.id,
+    article_id: Article.all.sample.id
+  )
+end
